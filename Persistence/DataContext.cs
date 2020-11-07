61cf6944c6c829cea055af05686b281e48149d15
Vlad Buzan <vladbuzan98@gmail.com>
2020-11-07 18:15:57 +0200

    Initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        // DbContext is a session with the database and is used to query the database and save instances of the entities 
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        // this will be used for the table name in sqlite
+        public DbSet<Value> Values {get; set; }
+        
+        //this will seed the database with some values
+        protected override void OnModelCreating(ModelBuilder builder) 
+        {
+            builder.Entity<Value>().HasData( 
+                new Value {Id = 1, Name = "Value 101"},
+                new Value {Id = 2, Name = "Value 102"},
+                new Value {Id = 3, Name = "Value 103"}
+            );
+        }
+
+    }
+}
